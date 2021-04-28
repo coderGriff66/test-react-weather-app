dedc325193b1b749c0a176aaa88498665480763e
coderGriff66 <77123924+coderGriff66@users.noreply.github.com>
2021-04-27 20:24:50 -0400

    initial setup, add bootstrap, axios footer with GitHub link, styles.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="container">
+        <div className="WeatherApp">
+          
+                   
+
+          <footer>
+            This project coded by Jennifer Griffin and is 
+            <a href="https://github.com/coderGriff66/test-react-weather-app" target="_blank" rel="noreferrer"> open-sourced on GitHub</a>
+          </footer>
+        </div>
+      </div>
     </div>
   );
 }
