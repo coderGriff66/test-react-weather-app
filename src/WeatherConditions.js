56288715819799b50881baf4d625696d9eee5910
coderGriff66 <77123924+coderGriff66@users.noreply.github.com>
2021-05-04 17:44:49 -0400

    Changed Wording for better fit

--- src/WeatherConditions.js
+++ src/WeatherConditions.js
@@ -8,7 +8,7 @@ export default function WeatherConditions(props) {
   return (
     <div className="WeatherConditions">
       <div className="row">
-          <div className="col-6">
+          <div className="col-7">
             <div className="Card Leftside">
               <h2 className="text-capitalize">{props.data.description}</h2>
                 <div className="clearfix">
@@ -28,7 +28,7 @@ export default function WeatherConditions(props) {
                   <li>Feels Like: <strong>{Math.round(props.data.feelsLike)}°</strong></li>
                   <li>Humidity: <strong>{props.data.humidity}%</strong></li>
                   <li>Wind: <strong>{Math.round(props.data.winds)} km/h</strong></li>
-                  <li>Today's Low Temp: <strong>{Math.round(props.data.tempMin)}°</strong></li>
+                  <li>Low Temp: <strong>{Math.round(props.data.tempMin)}°</strong></li>
                 </ul>
               </div>
             </div>
